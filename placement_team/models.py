f8b01e05202bfa6b7209fb1c13dab6e4f81a4e0e
ArjunThazhath <146722372+arjunthazath@users.noreply.github.com>
2025-03-07 17:57:44 +0530

    recruiters model created

--- placement_team/models.py
+++ placement_team/models.py
@@ -3,6 +3,8 @@ from django.utils.timezone import now
 # Create your models here.
 from django.db.models import UniqueConstraint
 
+from recruiters.models import Recruiter
+
 class Job_fairs(models.Model):
     job_fair_id = models.AutoField(primary_key=True)
     district = models.CharField(max_length=100)
@@ -15,17 +17,6 @@ class Job_fairs(models.Model):
     def __str__(self):
         return f"{self.district} - {self.date_of_job_fair}"
 
-class Recruiter(models.Model):
-    recruiter_id = models.AutoField(primary_key=True)
-    recruiter_email = models.EmailField(max_length=255,unique=True)
-    recruiter_password = models.CharField(max_length=255)
-    recruiter_qr_code = models.ImageField(upload_to='recruiter_qr_codes/', null=True, blank=True)
-
-    def __str__(self):
-        return self.recruiter_email
-    
-    class Meta:
-        db_table = 'recruiter'
 
 class RecruiterJobFair(models.Model):
     recruiter = models.ForeignKey(Recruiter, on_delete=models.CASCADE, related_name='recruiter_job_fairs')
--- recruiters/models.py
+++ recruiters/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Recruiter(models.Model):
+    recruiter_id = models.AutoField(primary_key=True)
+    recruiter_email = models.EmailField(max_length=255,unique=True)
+    recruiter_password = models.CharField(max_length=255)
+    recruiter_qr_code = models.ImageField(upload_to='recruiter_qr_codes/', null=True, blank=True)
+
+    def __str__(self):
+        return self.recruiter_email
+    
+    class Meta:
+        db_table = 'recruiter'
