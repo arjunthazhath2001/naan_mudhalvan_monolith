df64a2dab574dfec2be27d857a3806ca063ef364
ArjunThazhath <146722372+arjunthazath@users.noreply.github.com>
2025-03-10 22:10:57 +0530

    media files setup ok

--- recruiters/models.py
+++ recruiters/models.py
@@ -5,8 +5,7 @@ class Recruiter(models.Model):
     recruiter_id = models.AutoField(primary_key=True)
     recruiter_email = models.EmailField(max_length=255,unique=True)
     recruiter_password = models.CharField(max_length=255)
-    recruiter_qr_code = models.ImageField(upload_to='recruiter_qr_codes/', null=True, blank=True)
-
+   
     def __str__(self):
         return self.recruiter_email
     
