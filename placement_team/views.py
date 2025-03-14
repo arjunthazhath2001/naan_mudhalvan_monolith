8b41a3d333d5e9548480ccd06899e47d6fdd6caa
ArjunThazhath <146722372+arjunthazath@users.noreply.github.com>
2025-03-14 19:52:00 +0530

    ok

--- placement_team/views.py
+++ placement_team/views.py
@@ -39,7 +39,7 @@ def generate_qr_code_image(job_fair_id, district):
                        error_correction=qrcode.constants.ERROR_CORRECT_L, 
                        box_size=8,  # Reduced from 10 to make room for text
                        border=4)
-    public_url = 'http://13.49.75.11:8000'  # Replace with your EC2 instance's public IP or domain
+    public_url = 'http://ec2-13-49-75-11.eu-north-1.compute.amazonaws.com:8000'
     registration_link = f"{public_url}/nm/students/register/{job_fair_id}/"
     
     
@@ -236,7 +236,7 @@ def generate_recruiter_qr_code(job_fair_id, recruiter_id):
         box_size=8,
         border=4
     )
-    public_url = 'http://13.49.75.11:8000'  # Replace with your EC2 instance's public IP or domain
+    public_url = 'http://ec2-13-49-75-11.eu-north-1.compute.amazonaws.com:8000'
     attendance_url = f"{public_url}/nm/students/mark-attendance/{job_fair_id}/{recruiter_id}/"
 
     
